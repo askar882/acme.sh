ba1a74eda3a0071012e964fd5738e717cd6edb5c
askar882 <17901680+askar882@users.noreply.github.com>
2025-02-11 22:41:11 +0800

    Switch to DNSPod API 3.0 since they're deprecating old API.

--- dnsapi/dns_dp.sh
+++ dnsapi/dns_dp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# shellcheck disable=SC2034
+# shellcheck disable=SC2034,SC2059,SC2181
 dns_dp_info='DNSPod.cn
 Site: DNSPod.cn
 Docs: github.com/acmesh-official/acme.sh/wiki/dnsapi#dns_dp
@@ -7,8 +7,6 @@ Options:
  DP_Id Id
  DP_Key Key
 '
-
-REST_API="https://dnsapi.cn"
 
 ########  Public functions #####################
 
@@ -31,14 +29,13 @@ dns_dp_add() {
   _saveaccountconf_mutable DP_Id "$DP_Id"
   _saveaccountconf_mutable DP_Key "$DP_Key"
 
-  _debug "First detect the root zone"
+  _debug "First detect the root zone."
   if ! _get_root "$fulldomain"; then
-    _err "invalid domain"
+    _err "Invalid domain."
     return 1
   fi
 
-  add_record "$_domain" "$_sub_domain" "$txtvalue"
-
+  add_record "$txtvalue"
 }
 
 #fulldomain txtvalue
@@ -51,51 +48,49 @@ dns_dp_rm() {
 
   _debug "First detect the root zone"
   if ! _get_root "$fulldomain"; then
-    _err "invalid domain"
+    _err "Invalid domain."
     return 1
   fi
 
-  if ! _rest POST "Record.List" "login_token=$DP_Id,$DP_Key&format=json&lang=en&domain_id=$_domain_id&sub_domain=$_sub_domain"; then
-    _err "Record.Lis error."
+  if ! _rest "DescribeRecordList" "{\"Domain\":\"$_domain\"}"; then
+    _err "DescribeRecordList error."
     return 1
   fi
 
-  if _contains "$response" 'No records'; then
+  if _contains "$response" 'ResourceNotFound.NoDataOfRecord'; then
     _info "Don't need to remove."
     return 0
   fi
 
-  record_id=$(echo "$response" | tr "{" "\n" | grep -- "$txtvalue" | grep '^"id"' | cut -d : -f 2 | cut -d '"' -f 2)
+  record_id=$(echo "$response" | tr "{" "\n" | grep -- "$txtvalue" | _egrep_o "\"RecordId\":[0-9]*" | cut -d : -f 2)
   _debug record_id "$record_id"
   if [ -z "$record_id" ]; then
     _err "Can not get record id."
     return 1
   fi
 
-  if ! _rest POST "Record.Remove" "login_token=$DP_Id,$DP_Key&format=json&lang=en&domain_id=$_domain_id&record_id=$record_id"; then
-    _err "Record.Remove error."
+  if ! _rest "DeleteRecord" "{\"Domain\":\"$_domain\",\"RecordId\":$record_id}"; then
+    _err "DeleteRecord error."
     return 1
   fi
 
-  _contains "$response" "successful"
-
+  ! _contains "$response" "Error"
 }
 
 #add the txt record.
-#usage: root  sub  txtvalue
+#usage: txtvalue
 add_record() {
-  root=$1
-  sub=$2
-  txtvalue=$3
-  fulldomain="$sub.$root"
+  txtvalue=$1
+  fulldomain="$_sub_domain.$_domain"
 
   _info "Adding record"
 
-  if ! _rest POST "Record.Create" "login_token=$DP_Id,$DP_Key&format=json&lang=en&domain_id=$_domain_id&sub_domain=$_sub_domain&record_type=TXT&value=$txtvalue&record_line=%E9%BB%98%E8%AE%A4"; then
+  if ! _rest "CreateTXTRecord" "{\"Domain\":\"$_domain\",\"RecordLine\":\"默认\",\"Value\":\"$txtvalue\",\"SubDomain\":\"$_sub_domain\"}"; then
+    _err "CreateTXTRecord error."
     return 1
   fi
 
-  _contains "$response" "successful" || _contains "$response" "Domain record already exists"
+  _contains "$response" "RecordId" || _contains "$response" "InvalidParameter.DomainRecordExist"
 }
 
 ####################  Private functions below ##################################
@@ -103,27 +98,27 @@ add_record() {
 #returns
 # _sub_domain=_acme-challenge.www
 # _domain=domain.com
-# _domain_id=sdjkglgdfewsdfg
+# _domain_id=88888888
 _get_root() {
   domain=$1
   i=2
   p=1
   while true; do
-    h=$(printf "%s" "$domain" | cut -d . -f "$i"-100)
+    h=$(printf "$domain" | cut -d . -f "$i"-100)
     if [ -z "$h" ]; then
       #not valid
       return 1
     fi
 
-    if ! _rest POST "Domain.Info" "login_token=$DP_Id,$DP_Key&format=json&lang=en&domain=$h"; then
+    if ! _rest "DescribeDomain" "{\"Domain\":\"$h\"}"; then
       return 1
     fi
 
-    if _contains "$response" "successful"; then
-      _domain_id=$(printf "%s\n" "$response" | _egrep_o "\"id\":\"[^\"]*\"" | cut -d : -f 2 | tr -d \")
+    if _contains "$response" "DomainInfo"; then
+      _domain_id=$(echo "$response" | _egrep_o "\"DomainId\":[0-9]*" | cut -d : -f 2)
       _debug _domain_id "$_domain_id"
       if [ "$_domain_id" ]; then
-        _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-"$p")
+        _sub_domain=$(printf '%s' "$domain" | cut -d . -f 1-"$p")
         _debug _sub_domain "$_sub_domain"
         _domain="$h"
         _debug _domain "$_domain"
@@ -137,25 +132,48 @@ _get_root() {
   return 1
 }
 
-#Usage: method  URI  data
+#Usage: action  payload
 _rest() {
-  m="$1"
-  ep="$2"
-  data="$3"
-  _debug "$ep"
-  url="$REST_API/$ep"
+  action="$1"
+  payload="$2"
+  _debug "$action"
 
-  _debug url "$url"
+  service="dnspod"
+  host="dnspod.tencentcloudapi.com"
+  version="2021-03-23"
+  algorithm="TC3-HMAC-SHA256"
+  timestamp=$(date +%s)
+  date=$(date -u -d @"$timestamp" +"%Y-%m-%d")
 
-  if [ "$m" = "GET" ]; then
-    response="$(_get "$url" | tr -d '\r')"
-  else
-    _debug2 data "$data"
-    response="$(_post "$data" "$url" | tr -d '\r')"
-  fi
+  # 1. Splice standard request string.
+  http_request_method="POST"
+  canonical_uri="/"
+  canonical_querystring=""
+  canonical_headers="content-type:application/json; charset=utf-8\nhost:$host\nx-tc-action:$(echo "$action" | awk '{print tolower($0)}')\n"
+  signed_headers="content-type;host;x-tc-action"
+  hashed_request_payload=$(printf "$payload" | openssl sha256 -hex | awk '{print $2}')
+  canonical_request="POST\n$canonical_uri\n$canonical_querystring\n$canonical_headers\n$signed_headers\n$hashed_request_payload"
+
+  # 2. Splice string to be signed.
+  credential_scope="$date/$service/tc3_request"
+  hashed_canonical_request=$(printf "$canonical_request" | openssl sha256 -hex | awk '{print $2}')
+  string_to_sign="$algorithm\n$timestamp\n$credential_scope\n$hashed_canonical_request"
+
+  # 3. Calculate signature.
+  secret_date=$(printf "$date" | openssl sha256 -hmac "TC3$DP_Key" | awk '{print $2}')
+  secret_service=$(printf $service | openssl dgst -sha256 -mac hmac -macopt hexkey:"$secret_date" | awk '{print $2}')
+  secret_signing=$(printf 'tc3_request' | openssl dgst -sha256 -mac hmac -macopt hexkey:"$secret_service" | awk '{print $2}')
+  signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac hmac -macopt hexkey:"$secret_signing" | awk '{print $2}')
+
+  # 4. Splice Authorization header.
+  authorization="$algorithm Credential=$DP_Id/$credential_scope, SignedHeaders=$signed_headers, Signature=$signature"
+
+  # 5. Initiate request.
+  _debug2 payload "$payload"
+  response="$(_H1="Authorization: $authorization" _H2="Host: $host" _H3="X-TC-Action: $action" _H4="X-TC-Timestamp: $timestamp" _H5="X-TC-Version: $version" _post "$payload" "https://$host" "" "" "application/json; charset=utf-8" | tr -d '\r')"
 
   if [ "$?" != "0" ]; then
-    _err "error $ep"
+    _err "Error in $action"
     return 1
   fi
   _debug2 response "$response"
